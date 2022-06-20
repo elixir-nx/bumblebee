9635ac834eb66d28ca8a6ee8805ad466ce260179
Sean Moriarity <smoriarity.5@gmail.com>
2022-06-20 11:38:04 -0400

    Apply suggestions from code review

--- lib/bumblebee/vision/convnext.ex
+++ lib/bumblebee/vision/convnext.ex
@@ -3,38 +3,56 @@ defmodule Bumblebee.Vision.ConvNext do
 
   @moduledoc """
   Model based on the ConvNeXT architecture.
+
   ## Architectures
+
     * `:base` - plain ConvNeXT without any head on top
+
     * `:for_image_classification` - ConvNeXT with a classification head.
       The head consists of a single dense layer on top of the pooled
       features
+
   ## Configuration
+
     * `:num_channels` - the number of input channels. Defaults to `3`
+
     * `:patch_size` - patch size to use in the embedding layer. Defaults
       to `4`
+
     * `:num_stages` - the number of stages of the model. Defaults to `4`
+
     * `:hidden_sizes` - dimensionality (hidden size) at each stage.
       Defaults to `[96, 192, 384, 768]`
+
     * `:depths` - depth (number of layers) for each stage. Defaults
       to `[3, 3, 9, 3]`
+
     * `:hidden_act` - the activation function in each block. Defaults
       to `:gelu`
+
     * `:initializer_range` - standard deviation of the truncated normal
       initializer for initializing weight matrices. Defaults to `0.02`
+
     * `:layer_norm_eps` - epsilon value used by layer normalization layers.
       Defaults to `1.0e-12`
-    * `:layer_norm_init_value` - initial value for layer normalization scale.
+
+    * `:layer_scale_init_value` - initial value for layer normalization scale.
       Defaults to `1.0e-6`
+
     * `:drop_path_rate` - drop path rate for stochastic depth. Defaults to
       `0.0`
+
   ### Common Options
+
   #{Bumblebee.Shared.common_config_docs(@common_keys)}
+
   ## References
+
     * [A ConvNet for the 2020s](https://arxiv.org/abs/2201.03545)
   """
 
   alias Bumblebee.Shared
-  import Nx.Defn
+  alias Bumblebee.Layers
 
   defstruct [
               architecture: :base,
@@ -66,26 +84,29 @@ defmodule Bumblebee.Vision.ConvNext do
 
   @impl true
   def model(%__MODULE__{architecture: :for_image_classification} = config) do
-    convnext = base_model(config, "convnext")
-
-    outputs = Bumblebee.Utils.Axon.unwrap_container(convnext)
+    outputs = convnext(config, name: "convnext")
 
     logits =
       outputs.pooler_output
-      |> Axon.dense(config.num_labels, name: "classifier")
+      |> Axon.dense(config.num_labels,
+        name: "classifier",
+        kernel_initializer: kernel_initializer(config)
+      )
 
     Axon.container(%{logits: logits, hidden_states: outputs.hidden_states})
   end
 
   @impl true
   def model(%__MODULE__{architecture: :base} = config) do
-    base_model(config, "convnext")
+    config
+    |> convnext(name: "convnext")
+    |> Axon.container()
   end
 
-  defp base_model(config, name) do
-    pixel_values = Axon.input({nil, config.num_channels, 224, 224}, "pixel_values")
+  defp convnext(config, opts) do
+    name = opts[:name]
 
-    # TODO: Correct initializers
+    pixel_values = Axon.input({nil, config.num_channels, 224, 224}, "pixel_values")
 
     embedding_output = embeddings(pixel_values, config, name: join(name, "embeddings"))
 
@@ -95,13 +116,18 @@ defmodule Bumblebee.Vision.ConvNext do
     pooled_output =
       last_hidden_state
       |> Axon.global_avg_pool()
-      |> Axon.layer_norm(epsilon: config.layer_norm_eps, name: join(name, "layernorm"))
+      |> Axon.layer_norm(
+        epsilon: config.layer_norm_eps,
+        name: join(name, "layernorm"),
+        beta_initializer: :zeros,
+        gamma_initializer: :ones
+      )
 
-    Axon.container(%{
+    %{
       last_hidden_state: last_hidden_state,
       pooler_output: pooled_output,
       hidden_states: hidden_states
-    })
+    }
   end
 
   defp embeddings(%Axon{} = pixel_values, config, opts) do
@@ -112,9 +138,15 @@ defmodule Bumblebee.Vision.ConvNext do
     |> Axon.conv(embedding_size,
       kernel_size: config.patch_size,
       strides: config.patch_size,
-      name: join(name, "patch_embeddings")
+      name: join(name, "patch_embeddings"),
+      kernel_initializer: kernel_initializer(config)
     )
-    |> Axon.layer_norm(epsilon: 1.0e-6, name: join(name, "layernorm"))
+    |> Axon.layer_norm(
+      epsilon: 1.0e-6,
+      name: join(name, "layernorm"),
+      beta_initializer: :zeros,
+      gamma_initializer: :ones
+    )
   end
 
   defp encoder(hidden_states, config, opts) do
@@ -124,12 +156,13 @@ defmodule Bumblebee.Vision.ConvNext do
     last_hidden_state = hidden_states
     all_hidden_states = {last_hidden_state}
 
-    for idx <- 0..(config.num_stages - 1), reduce: {last_hidden_state, all_hidden_states} do
+    stages =
+      Enum.zip([0..(config.num_stages - 1), config.depths, drop_path_rates, config.hidden_sizes])
+
+    for {idx, depth, drop_path_rates, out_channels} <- stages,
+        reduce: {last_hidden_state, all_hidden_states} do
       {lhs, states} ->
         strides = if idx > 0, do: 2, else: 1
-        depth = Enum.at(config.depths, idx)
-        drop_path_rates = Enum.at(drop_path_rates, idx)
-        out_channels = Enum.at(config.hidden_sizes, idx)
 
         stage_name = join("stages", "#{idx}")
 
@@ -160,23 +193,32 @@ defmodule Bumblebee.Vision.ConvNext do
     downsampled =
       if in_channels != out_channels or strides > 1 do
         hidden_states
-        |> Axon.layer_norm(epsilon: 1.0e-6, name: join(name, "downsampling_layer.0"))
+        |> Axon.layer_norm(
+          epsilon: 1.0e-6,
+          name: join(name, "downsampling_layer.0"),
+          beta_initializer: :zeros,
+          gamma_initializer: :ones
+        )
         |> Axon.conv(out_channels,
           kernel_size: 2,
           strides: strides,
-          name: join(name, "downsampling_layer.1")
+          name: join(name, "downsampling_layer.1"),
+          kernel_initializer: kernel_initializer(config)
         )
       else
         hidden_states
       end
 
-    for idx <- 0..(depth - 1), reduce: downsampled do
+    # This is essentially the same as calling `with_index`, but
+    # will ensure that we error out if for some reason we didn't
+    # compute drop_path_rates right (e.g. it doesn't match the depth)
+    for {drop_path_rate, idx} <- Enum.zip(drop_path_rates, 0..(depth - 1)), reduce: downsampled do
       x ->
         layer_name = join("layers", "#{idx}")
 
         conv_next_layer(x, out_channels, config,
           name: join(name, layer_name),
-          drop_path_rate: Enum.at(drop_path_rates, idx)
+          drop_path_rate: drop_path_rate
         )
     end
   end
@@ -194,26 +236,33 @@ defmodule Bumblebee.Vision.ConvNext do
       |> Axon.depthwise_conv(channel_multiplier,
         kernel_size: 7,
         padding: [{3, 3}, {3, 3}],
-        name: join(name, "dwconv")
+        name: join(name, "dwconv"),
+        kernel_initializer: kernel_initializer(config)
       )
       |> Axon.transpose([0, 2, 3, 1], ignore_batch?: false, name: join(name, "transpose1"))
-      |> Axon.layer_norm(epsilon: 1.0e-6, channel_index: 3, name: join(name, "layernorm"))
-      |> Axon.dense(4 * out_channels, name: join(name, "pwconv1"))
+      |> Axon.layer_norm(
+        epsilon: 1.0e-6,
+        channel_index: 3,
+        name: join(name, "layernorm"),
+        beta_initializer: :zeros,
+        gamma_initializer: :ones
+      )
+      |> Axon.dense(4 * out_channels,
+        name: join(name, "pwconv1"),
+        kernel_initializer: kernel_initializer(config)
+      )
       |> Axon.activation(config.hidden_act, name: join(name, "activation"))
-      |> Axon.dense(out_channels, name: join(name, "pwconv2"))
+      |> Axon.dense(out_channels,
+        name: join(name, "pwconv2"),
+        kernel_initializer: kernel_initializer(config)
+      )
 
     scaled =
       if config.layer_scale_init_value > 0 do
-        scale =
-          Axon.param("layer_scale_parameter", {out_channels},
-            initializer: fn shape, _ ->
-              Nx.broadcast(config.layer_scale_init_value, shape)
-            end
-          )
-
-        Axon.layer(fn x, y, _opts -> Nx.multiply(x, y) end, [x, scale],
+        Layers.scale_layer(x,
           name: name,
-          op_name: :scale
+          scale_init_value: config.layer_scale_init_value,
+          channel_index: 3
         )
       else
         x
@@ -221,44 +270,8 @@ defmodule Bumblebee.Vision.ConvNext do
 
     scaled
     |> Axon.transpose([0, 3, 1, 2], ignore_batch?: false, name: join(name, "transpose2"))
-    |> drop_path_layer(rate: drop_path_rate, name: join(name, "drop_path"))
+    |> Layers.drop_path_layer(rate: drop_path_rate, name: join(name, "drop_path"))
     |> Axon.add(input, name: join(name, "residual"))
-  end
-
-  defp drop_path_layer(%Axon{} = input, opts) do
-    opts = Keyword.validate!(opts, [:name, rate: 0.0])
-
-    if opts[:rate] > 0.0 do
-      Axon.layer(&drop_path/2, [input], name: opts[:name], rate: opts[:rate])
-    else
-      input
-    end
-  end
-
-  defnp drop_path(x, opts \\ []) do
-    opts = keyword!(opts, rate: 0.0, mode: :train)
-
-    transform({x, opts[:rate], opts[:mode]}, fn
-      {x, rate, :train} ->
-        keep_prob = 1 - rate
-        shape = elem(Nx.shape(x), 0)
-
-        random_tensor =
-          keep_prob
-          |> Nx.add(Nx.random_uniform(shape))
-          |> Nx.floor()
-
-        out = x |> Nx.divide(keep_prob) |> Nx.multiply(random_tensor)
-        # Do not apply if rate is 0.0
-        if Elixir.Kernel.==(rate, 0.0) do
-          x
-        else
-          out
-        end
-
-      {x, _rate, :inference} ->
-        x
-    end)
   end
 
   defp get_channels(%Axon{output_shape: shape}) do
@@ -288,6 +301,10 @@ defmodule Bumblebee.Vision.ConvNext do
       end
 
     Enum.reverse(final_rates)
+  end
+
+  defp kernel_initializer(config) do
+    Axon.Initializers.normal(scale: config.initializer_range)
   end
 
   defp join(lhs, rhs), do: lhs <> "." <> rhs
