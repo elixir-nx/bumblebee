9f5b47399881a1eadaa4c46fd053e465263c5af5
Mateusz <msluszniak@student.agh.edu.pl>
2022-07-08 02:07:36 +0200

    Add roberta tokenizer test

---
+++ test/bumblebee/text/roberta_tokenizer_test.exs
@@ -0,0 +1,44 @@
+defmodule Bumblebee.Text.RoBERTaTokenizerTest do
+  use ExUnit.Case, async: true
+
+  import Bumblebee.TestHelpers
+
+  describe "integration" do
+    @tag :slow
+    test "encoding model input" do
+      assert {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "roberta-base"})
+
+      assert %Bumblebee.Text.RoBERTaTokenizer{} = tokenizer
+
+      inputs =
+        Bumblebee.apply_tokenizer(tokenizer, [
+          "Test sentence with <mask>.",
+          {"Question?", "Answer"}
+        ])
+
+      assert_equal(
+        inputs["input_ids"],
+        Nx.tensor([
+          [0, 34603, 3645, 19, 50264, 4, 2],
+          [0, 45641, 116, 2, 2, 33683, 2]
+        ])
+      )
+
+      assert_equal(
+        inputs["attention_mask"],
+        Nx.tensor([
+          [1, 1, 1, 1, 1, 1, 1],
+          [1, 1, 1, 1, 1, 1, 1]
+        ])
+      )
+
+      assert_equal(
+        inputs["token_type_ids"],
+        Nx.tensor([
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0]
+        ])
+      )
+    end
+  end
+end
