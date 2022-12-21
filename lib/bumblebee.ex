76ac28c6455e18b52ddadc5d809355f97e3507de
Sean Moriarity <smoriarity.5@gmail.com>
2022-12-21 13:51:33 -0500

    Add CamemBERT (#110)

--- lib/bumblebee.ex
+++ lib/bumblebee.ex
@@ -56,6 +56,14 @@ defmodule Bumblebee do
     "BertForTokenClassification" => {Bumblebee.Text.Bert, :for_token_classification},
     "BertLMHeadModel" => {Bumblebee.Text.Bert, :for_causal_language_modeling},
     "BertModel" => {Bumblebee.Text.Bert, :base},
+    # these models are just Roberta models, but the config will list them as camembert
+    "CamembertModel" => {Bumblebee.Text.Roberta, :base},
+    "CamembertForMaskedLM" => {Bumblebee.Text.Roberta, :for_masked_language_modeling},
+    "CamembertForSequenceClassification" =>
+      {Bumblebee.Text.Roberta, :for_sequence_classification},
+    "CamembertForMultipleChoice" => {Bumblebee.Text.Roberta, :for_multiple_choice},
+    "CamembertForTokenClassification" => {Bumblebee.Text.Roberta, :for_token_classification},
+    "CamembertForQuestionAnswering" => {Bumblebee.Text.Roberta, :for_question_answering},
     "CLIPModel" => {Bumblebee.Multimodal.Clip, :base},
     "CLIPTextModel" => {Bumblebee.Text.ClipText, :base},
     "CLIPVisionModel" => {Bumblebee.Vision.ClipVision, :base},
@@ -120,6 +128,7 @@ defmodule Bumblebee do
     "albert" => Bumblebee.Text.AlbertTokenizer,
     "bart" => Bumblebee.Text.BartTokenizer,
     "bert" => Bumblebee.Text.BertTokenizer,
+    "camembert" => Bumblebee.Text.CamembertTokenizer,
     "clip" => Bumblebee.Text.ClipTokenizer,
     "gpt2" => Bumblebee.Text.Gpt2Tokenizer,
     "layoutlm" => Bumblebee.Text.LayoutLmTokenizer,
---
+++ test/bumblebee/text/camembert_test.exs
@@ -0,0 +1,55 @@
+defmodule Bumblebee.Text.CamembertTest do
+  use ExUnit.Case, async: false
+
+  import Bumblebee.TestHelpers
+
+  @moduletag model_test_tags()
+
+  describe "integration" do
+    test "base model" do
+      assert {:ok, %{model: model, params: params, spec: spec}} =
+               Bumblebee.load_model({:hf, "camembert-base"}, architecture: :base)
+
+      assert %Bumblebee.Text.Roberta{architecture: :base} = spec
+
+      inputs = %{
+        "input_ids" => Nx.tensor([[0, 402, 232, 328, 740, 1140, 12695, 69, 1588, 2]])
+      }
+
+      outputs = Axon.predict(model, params, inputs)
+
+      assert Nx.shape(outputs.hidden_state) == {1, 10, 768}
+
+      assert_all_close(
+        outputs.hidden_state[[0..-1//1, 0..2, 0..2]],
+        Nx.tensor([
+          [[0.0592, 0.0688, 0.0185], [0.0024, 0.1443, -0.1943], [0.0102, 0.2724, -0.2474]]
+        ]),
+        atol: 1.0e-4
+      )
+    end
+
+    test "masked language modeling model" do
+      assert {:ok, %{model: model, params: params, spec: spec}} =
+               Bumblebee.load_model({:hf, "camembert-base"})
+
+      assert %Bumblebee.Text.Roberta{architecture: :for_masked_language_modeling} = spec
+
+      inputs = %{
+        "input_ids" => Nx.tensor([[0, 402, 232, 328, 740, 1140, 12695, 69, 1588, 2]])
+      }
+
+      outputs = Axon.predict(model, params, inputs)
+
+      assert Nx.shape(outputs.logits) == {1, 10, 32005}
+
+      assert_all_close(
+        outputs.logits[[0..-1//1, 0..2, 0..2]],
+        Nx.tensor([
+          [[18.4213, -4.5504, 6.5444], [-1.2791, -2.4822, 2.8339], [-2.5561, -4.3118, -1.3791]]
+        ]),
+        atol: 1.0e-4
+      )
+    end
+  end
+end
