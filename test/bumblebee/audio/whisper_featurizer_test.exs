92fc7256804f5eee5dbbdfc9b2215efabbf19612
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-01-24 21:38:22 +0100

    Fix test

--- test/bumblebee/audio/whisper_featurizer_test.exs
+++ test/bumblebee/audio/whisper_featurizer_test.exs
@@ -3,8 +3,6 @@ defmodule Bumblebee.Audio.WhisperFeaturizerTest do
 
   import Bumblebee.TestHelpers
 
-  require IEx
-
   describe "integration" do
     test "encoding model input" do
       assert {:ok, featurizer} = Bumblebee.load_featurizer({:hf, "openai/whisper-tiny.en"})
@@ -14,9 +12,9 @@ defmodule Bumblebee.Audio.WhisperFeaturizerTest do
       audio = Nx.from_numpy("test/fixtures/inputs/whisper/test_input_raw_1.npy")
       expected = Nx.from_numpy("test/fixtures/inputs/whisper/test_output_featurized_1.npy")
 
-      actual = Bumblebee.apply_featurizer(featurizer, audio)[0]
+      inputs = Bumblebee.apply_featurizer(featurizer, audio)
 
-      assert_all_close(actual, expected, atol: 1.0e-4, rtol: 1.0e-3)
+      assert_all_close(inputs["input_features"][0], expected, atol: 1.0e-4, rtol: 1.0e-3)
     end
   end
 end
