a3d3a75173ffcb2ecc9b69500763c9e723574ecf
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-07-26 15:56:43 -0300

    Update versions on CI (#224)
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- lib/bumblebee/shared.ex
+++ lib/bumblebee/shared.ex
@@ -260,7 +260,7 @@ defmodule Bumblebee.Shared do
           function(),
           keyword(),
           boolean(),
-          (() -> list(Nx.Tensor.t()))
+          (-> list(Nx.Tensor.t()))
         ) :: function()
   def compile_or_jit(fun, defn_options, compile?, template_fun) do
     if compile? do
