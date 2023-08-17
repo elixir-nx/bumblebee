df837047192228e63a0cafba0020036a1b3a56d0
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-08-17 16:09:07 +0200

    Release v0.3.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Bumblebee.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @description "Pre-trained and transformer Neural Network models in Axon"
 
   def project do
