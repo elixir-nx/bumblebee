d08d78fbfef7908a95ccded6c950529b26502e25
Ahmed Ismail <iamahmedismail19@gmail.com>
2023-11-14 00:19:10 +0500

    Add credo 1.7 to elixir-nx/bumblebee

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Bumblebee.MixProject do
       {:stb_image, "~> 0.6.0", only: :test},
       {:bypass, "~> 2.1", only: :test},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:nx_signal, "~> 0.2.0"}
+      {:nx_signal, "~> 0.2.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
