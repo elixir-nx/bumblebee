8f406558f0657e35698f62285747d5578b9fc861
George Guimarães <george.guimaraes@gmail.com>
2026-01-04 19:24:39 -0300

    test: Add :base architecture test for ModernBertDecoder

--- test/bumblebee/text/modernbert_decoder_test.exs
+++ test/bumblebee/text/modernbert_decoder_test.exs
@@ -5,6 +5,31 @@ defmodule Bumblebee.Text.ModernBertDecoderTest do
 
   @moduletag model_test_tags()
 
+  test ":base" do
+    assert {:ok, %{model: model, params: params, spec: spec}} =
+             Bumblebee.load_model(
+               {:hf, "onnx-internal-testing/tiny-random-ModernBertDecoderForCausalLM"},
+               architecture: :base
+             )
+
+    assert %Bumblebee.Text.ModernBertDecoder{architecture: :base} = spec
+
+    inputs = %{
+      "input_ids" => Nx.tensor([[10, 20, 30, 40, 50, 60, 70, 80, 0, 0]])
+    }
+
+    outputs = Axon.predict(model, params, inputs)
+
+    assert Nx.shape(outputs.hidden_state) == {1, 10, spec.hidden_size}
+
+    assert_all_close(
+      outputs.hidden_state[[.., 1..3, 1..3]],
+      Nx.tensor([
+        [[-0.5646, 0.0348, 2.0215], [-0.0864, -1.7016, 2.0514], [-1.2734, -0.2655, 1.8605]]
+      ])
+    )
+  end
+
   test ":for_causal_language_modeling" do
     assert {:ok, %{model: model, params: params, spec: spec}} =
              Bumblebee.load_model(
